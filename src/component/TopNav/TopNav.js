e81821e78ec9b802baec0cbf456b756c610ff575
Kakon Ray <thisiskakonray@gmail.com>
2022-03-25 14:00:03 +0600

    Top Navbar Design

--- src/component/TopNav/TopNav.js
+++ src/component/TopNav/TopNav.js
@@ -3,14 +3,14 @@ import "./TopNav.css";
 
 const TopNav = () => {
   return (
-    <div className="bg-light">
-      <div className="container-fluid">
-        <div className="d-flex justify-content-between">
-          <div>
-            <p>Special collection already available</p>
+    <div className="top-nav">
+      <div className="container-fluid py-2">
+        <div className="d-flex justify-content-between align-items-center ">
+          <div className="align-self-center">
+            <p className="my-0">Special collection already available</p>
           </div>
           <div className="topnav-list ">
-            <ul className="d-flex">
+            <ul className="d-flex align-self-center my-0">
               <li>Compare</li>
               <li>Login</li>
               <li>Register</li>
