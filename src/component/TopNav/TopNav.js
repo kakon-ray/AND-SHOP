c9027550941fa1f674cbc50d6ef075c3f2118c8d
Kakon Ray <thisiskakonray@gmail.com>
2022-03-25 14:26:34 +0600

    Top Nav Section completed

--- src/component/TopNav/TopNav.js
+++ src/component/TopNav/TopNav.js
@@ -1,5 +1,9 @@
 import React from "react";
 import "./TopNav.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCodeCompare } from "@fortawesome/free-solid-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
 
 const TopNav = () => {
   return (
@@ -7,13 +11,25 @@ const TopNav = () => {
       <div className="container-fluid py-2">
         <div className="d-flex justify-content-between align-items-center ">
           <div className="align-self-center">
-            <p className="my-0">Special collection already available</p>
+            <p className="my-0">
+              Special collection already available
+              <span className="readmore">Read More...</span>
+            </p>
           </div>
           <div className="topnav-list ">
             <ul className="d-flex align-self-center my-0">
-              <li>Compare</li>
-              <li>Login</li>
-              <li>Register</li>
+              <li>
+                <FontAwesomeIcon icon={faCodeCompare} className="me-1" />
+                Compare
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faUser} className="me-1" />
+                Login
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faLock} className="me-1" />
+                Register
+              </li>
             </ul>
           </div>
         </div>
