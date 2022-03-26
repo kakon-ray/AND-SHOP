b7cba5fadc61cd370265cce5f06eb2b4e66009de
Kakon Ray <thisiskakonray@gmail.com>
2022-03-26 19:46:32 +0600

    ofcanvas search bar style and customize

--- src/component/OffCanvas/OffCanvas.js
+++ src/component/OffCanvas/OffCanvas.js
@@ -1,25 +1,31 @@
 import React from "react";
-import { Offcanvas } from "react-bootstrap";
+import { Button, FormControl, InputGroup, Offcanvas } from "react-bootstrap";
+import "./OffCanvas.css";
 
 const OffCanvas = ({ onHide, placement, show }) => {
   return (
     <Offcanvas show={show} onHide={onHide} placement={placement}>
       <Offcanvas.Header closeButton>
-        <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        <Offcanvas.Title className="text-center mx-auto">
+          AND SHOP
+        </Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        Some text as placeholder. In real life you can have the elements you
-        have chosen. Like, text, images, lists, etc. Some text as placeholder.
-        In real life you can have the elements you have chosen. Like, text,
-        images, lists, etc. Some text as placeholder. In real life you can have
-        the elements you have chosen. Like, text, images, lists, etc. Some text
-        as placeholder. In real life you can have the elements you have chosen.
-        Like, text, images, lists, etc. Some text as placeholder. In real life
-        you can have the elements you have chosen. Like, text, images, lists,
-        etc. Some text as placeholder. In real life you can have the elements
-        you have chosen. Like, text, images, lists, etc. Some text as
-        placeholder. In real life you can have the elements you have chosen.
-        Like, text, images, lists, etc.
+        <InputGroup className="mb-3">
+          <FormControl
+            className="form-control-offcanvas"
+            placeholder="Search Your Fevorite Product"
+            aria-label="Recipient's username"
+            aria-describedby="basic-addon2"
+          />
+          <Button
+            variant="outline-secondary"
+            id="button-addon2 "
+            className="offcanvas-search-btn"
+          >
+            Search
+          </Button>
+        </InputGroup>
       </Offcanvas.Body>
     </Offcanvas>
   );
