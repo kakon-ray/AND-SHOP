c666132d20ea387612ecc0dd86fe46f7ef8ef548
Kakon Ray <thisiskakonray@gmail.com>
2022-04-05 01:35:14 +0600

    wish list add and removed

--- src/component/OffCanvas/WishListShow/WishListShow.js
+++ src/component/OffCanvas/WishListShow/WishListShow.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Button, FormControl, InputGroup, Offcanvas } from "react-bootstrap";
+import { wishListContext } from "../../../Contexts/wishListContext";
 
-const WishListShow = ({
-  handleClosWishLIst,
-  placement,
-  showWishList,
-  wishList,
-}) => {
-  // console.log(wishList);
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+
+const WishListShow = ({ handleClosWishLIst, placement, showWishList }) => {
+  const [wishList, setWishList] = useContext(wishListContext);
+
+  const removeItem = (id) => {
+    const removeThenCartItem = wishList.filter(
+      (listItem) => listItem?.item?.id !== id
+    );
+    setWishList(removeThenCartItem);
+    console.log(removeThenCartItem);
+  };
+
   return (
     <Offcanvas
       show={showWishList}
@@ -20,7 +28,34 @@ const WishListShow = ({
         </Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        <h1>This is show wishlist</h1>
+        {wishList.map((item) =>
+          item.item ? (
+            <div className="d-flex gap-3 my-2 align-items-center justify-content-between">
+              <div className="d-flex gap-3 my-2">
+                <div>
+                  <img
+                    src={item.item.img}
+                    alt=""
+                    className="img-fluid"
+                    style={{ width: "50px", height: "50px", border: "none" }}
+                  />
+                </div>
+                <div>
+                  <h6>{item.item.name}</h6>
+                  <p>Price: {item.item.price}</p>
+                </div>
+              </div>
+              {/* <Button variant="" className="navbar-icon pb-0 "></Button> */}
+              <FontAwesomeIcon
+                icon={faTrash}
+                size="lg"
+                onClick={() => removeItem(item.item.id)}
+              />
+            </div>
+          ) : (
+            ""
+          )
+        )}
       </Offcanvas.Body>
     </Offcanvas>
   );
