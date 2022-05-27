5ed753b3bd1fa3c3233dffa7189250721823ac42
Kakon Ray <thisiskakonray@gmail.com>
2022-05-27 12:51:15 +0600

    Shop menu responsive

--- src/Dashboard/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard/Dashboard.js
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router-dom";
 import DashboardSidebar from "../DashboardSidebar/DashboardSidebar";
+import "./Dashboard.css";
 
 const Dashboard = () => {
   return (
     <>
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-3 dashboard-lg-sidebar">
-            <DashboardSidebar />
+          <div className="col-md-3 ">
+            <div className="dashboard-lg-sidebar">
+              <DashboardSidebar />
+            </div>
           </div>
           <div className="col-md-9">
             <Outlet />
